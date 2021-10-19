24e13f3e94ae9128ccf768756e73a19fdc1e6b4a
burcMnt <burcumantar0@gmail.com>
2021-10-20 02:33:16 +0300

    Seed data and prepare UI

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+        public DemoDataAccess()
+        {
+            people.Add(item: new PersonModel { Id = 1, FirstName = "Ali", LastName = "Yılmaz" });
+            people.Add(item: new PersonModel { Id = 2, FirstName = "Can", LastName = "Gök" });
+            people.Add(item: new PersonModel { Id = 3, FirstName = "Cem", LastName = "Ateş" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+            p.Id = people.Max(x => x.Id) + 1;
+            people.Add(p);
+            return p;
+        }
+
+        public void DeletePerson(int id)
+        {
+            PersonModel p = people.Find(x => x.Id == id);
+            people.Remove(p);
+        }
+    }
+}
---
+++ DemoLibrary/DataAccess/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using DemoLibrary.Models;
+using System.Collections.Generic;
+
+namespace DemoLibrary.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<PersonModel> GetPeople();
+        PersonModel InsertPerson(string firstName, string lastName);
+        void DeletePerson(int id);
+    }
+}
